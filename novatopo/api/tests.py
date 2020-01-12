42422880ca066634864842f8f90035b03d8d8854
Felix Maillet <fmaillet@macbook-pro-de-felix.home>
2020-01-13 00:37:16 +0100

    Adding 2 tests, requirements.txt

--- novatopo/api/tests.py
+++ novatopo/api/tests.py
@@ -1,3 +1,75 @@
-from django.test import TestCase
+import graphene
+from django.test.testcases import TestCase
+from novatopo.schema import Query
 
-# Create your tests here.
+from api.models import Activity, Business
+
+
+def initialize():
+    obj = Activity(name='Karting')
+    obj.save()
+
+    objj = Activity(name='Acrobranche')
+    objj.save()
+
+    bus = Business(name='Le Mans Ask', address="Chemin aux Boeufs")
+    bus.save()
+
+    buss = Business(name='Acrobranche Air', address="Chemin aux Boeufs")
+    buss.save()
+
+
+class ApiTestCase(TestCase):
+
+    def setUp(self):
+        super().setUp()
+        initialize()
+
+    def test_all_activity(self):
+        query = """
+            query {
+              allActivity {
+                id
+                name
+              }
+            }
+        """
+        schema = graphene.Schema(query=Query)
+        result = schema.execute(query)
+        assert not result.errors
+        assert result.data == {"allActivity": [
+            {
+                "id": "1",
+                "name": "Karting"
+            },
+            {
+                "id": "2",
+                "name": "Acrobranche"
+            },
+        ]}
+
+    def test_all_business(self):
+        query = """
+            query {
+              allBusiness {
+                id
+                name
+                address
+              }
+            }
+        """
+        schema = graphene.Schema(query=Query)
+        result = schema.execute(query)
+        assert not result.errors
+        assert result.data == {"allBusiness": [
+            {
+                "id": "3",
+                "name": "Le Mans Ask",
+                "address": "Chemin aux Boeufs"
+            },
+            {
+                "id": "4",
+                "name": "Acrobranche Air",
+                "address": "Chemin aux Boeufs"
+            },
+        ]}
