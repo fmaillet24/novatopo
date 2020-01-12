c746fc1fafc71b6eb9d21dd81816caa57b99efbe
Felix Maillet <fmaillet@macbook-pro-de-felix.home>
2020-01-12 16:19:53 +0100

    Creating Booking Mutation

--- novatopo/api/models.py
+++ novatopo/api/models.py
@@ -30,8 +30,11 @@ class Booking(models.Model):
     name = models.CharField(null=False, max_length=255)
     email = models.CharField(null=False, max_length=255)
     participant = models.IntegerField()
-    date = models.DateField()
+    date = models.DateTimeField()
+    total = models.IntegerField(default=0)
     is_staff = models.BooleanField(default=False)
+    activity = models.ForeignKey(
+        Activity, on_delete=models.CASCADE, related_name='booking', default=1)
 
     def __str__(self):
         return self.name
--- novatopo/api/schema.py
+++ novatopo/api/schema.py
@@ -26,6 +26,50 @@ class BookingType(DjangoObjectType):
         model = Booking
 
 
+class CreateBooking(graphene.Mutation):
+    id = graphene.Int()
+    name = graphene.String()
+    email = graphene.String()
+    participant = graphene.Int()
+    date = graphene.types.datetime.DateTime()
+    total = graphene.Int()
+    is_staff = graphene.Boolean()
+    activity = graphene.String()
+
+    class Arguments:
+        name = graphene.String()
+        email = graphene.String()
+        participant = graphene.Int()
+        date = graphene.types.datetime.DateTime()
+        is_staff = graphene.Boolean()
+        activity = graphene.String()
+
+    def mutate(self, info, name, email, participant, date, is_staff, activity):
+        x = Activity.objects.get(name=activity)
+        if ActivityAvailable.objects.filter(activity=x, date=date).exists():
+            price = x.price
+            total = int(price) * int(participant)
+            booking = Booking(name=name, email=email, participant=participant,
+                              date=date, is_staff=is_staff, total=total, activity=x)
+            booking.save()
+        else:
+            raise Exception('Invalid Date')
+
+        return CreateBooking(
+            id=booking.id,
+            name=booking.name,
+            email=booking.email,
+            participant=booking.participant,
+            date=booking.date,
+            total=booking.total,
+            is_staff=booking.is_staff,
+            activity=booking.activity)
+
+
+class Mutation(graphene.ObjectType):
+    create_booking = CreateBooking.Field()
+
+
 class Query(object):
     all_activity = graphene.List(ActivityType)
     all_business = graphene.List(BusinessType)
@@ -36,5 +80,10 @@ class Query(object):
         return Activity.objects.all()
 
     def resolve_all_business(self, info, **kwargs):
-        # We can easily optimize query count in the resolve method
         return Business.objects.all()
+
+    def resolve_all_booking(self, info, **kwargs):
+        return Booking.objects.all()
+
+    def resolve_all_activity_available(self, info, **kwargs):
+        return ActivityAvailable.objects.all()
